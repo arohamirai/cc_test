56d2d89b4aaae6bee930bb6a7ac132ae2dad0c37
liufeng <wslf0115@126.com>
2018-10-11 14:06:51 +0800

    full useage for jsoncpp

--- src/cc_test.cpp
+++ src/cc_test.cpp
@@ -3,37 +3,61 @@
 #include <boost/make_shared.hpp>
 #include <iostream>
 #include <fstream>
+#include <sstream>
 #include <string>
 #include <jsoncpp/json/json.h>
 using namespace std;
 
+// jsoncpp  reference : https://www.cnblogs.com/DswCnblog/p/6678708.html
 
 int main(int argc, char **argv)
 {
-    ifstream json_file("../file/info_WorldRefer.json", ios::binary);
-    if(!json_file.is_open())
+    ifstream json_file_in("../file/info_WorldRefer.json", ios::binary);
+    ofstream json_file_out("../file/info_WorldRefer_out.json", ios::binary);
+
+    Json::Reader reader(Json::Features::strictMode());
+    Json::StyledWriter writer;
+    Json::Value root;
+
+
+    if(!json_file_in.is_open())
     {
         cout<<"can't open json file."<<endl;
         return 0;
     }
 
-    Json::Reader reader;
-    Json::Value root;
-    if(reader.parse(json_file, root))
+    if(reader.parse(json_file_in, root))
     {
-        cout<<"dictCategory: "<< root["dictCategory"]["0"]<<endl;
-        cout<<"dictEvent: "<< root["dictEvent"]["0"]<< "  "<< root["dictEvent"]["1"]<<endl;
-
-        //读取数组信息
-        cout << "directActions:" << endl;
-        for(unsigned int i = 0; i < root["directActions"].size(); i++)
+        cout << "nodes:" << endl;
+        for(unsigned int i = 0; i < root["nodes"].size(); i++)
         {
-            string ach = root["directActions"][i].asString();
-            cout << ach << endl;
+            // 修改json字段
+            for(unsigned int j = 0; j < root["nodes"][i]["pose"].size(); j++)
+            {
+                root["nodes"][i]["pose"][j] = 0;
+            }
+
+            // 读json字段
+            cout << "worldPose: "
+                 <<root["nodes"][i]["worldPose"]["position"]["x"].asFloat()<<"  "
+                 <<root["nodes"][i]["worldPose"]["position"]["x"].asFloat()<<"  "
+                 <<root["nodes"][i]["worldPose"]["position"]["y"].asFloat()<<"  "
+                 <<root["nodes"][i]["worldPose"]["position"]["z"].asFloat()<<"  "
+                 <<root["nodes"][i]["worldPose"]["orientation"]["x"].asFloat()<<"  "
+                 <<root["nodes"][i]["worldPose"]["orientation"]["y"].asFloat()<<"  "
+                 <<root["nodes"][i]["worldPose"]["orientation"]["z"].asFloat()<<"  "
+                 <<root["nodes"][i]["worldPose"]["orientation"]["w"].asFloat()<<"  ";
+            cout << endl;
+            cout<<"==========================="<<endl;
         }
     }
 
+    // 写json文件
+    string str_out = writer.write(root);
+    json_file_out << str_out;
 
+    json_file_in.close();
+    json_file_out.close();
 
     return 0;
 }
