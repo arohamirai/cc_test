0214063279eafb605ae4470bb7d2977fd1eb94af
liufeng <wslf0115@126.com>
2018-12-12 11:29:41 +0800

    example can run

--- src/cc_test.cpp
+++ src/cc_test.cpp
@@ -1,111 +1,73 @@
-#include <iostream>
-#include <opencv2/opencv.hpp>
-#include <visp3/core/vpHomogeneousMatrix.h>
-#include <visp3/core/vpVelocityTwistMatrix.h>
-#include <visp3/gui/vpPlot.h>
-#include <visp3/robot/vpSimulatorPioneer.h>
+#include <visp3/gui/vpDisplayGDI.h>
+#include <visp3/gui/vpDisplayOpenCV.h>
+#include <visp3/gui/vpDisplayX.h>
+#include <visp3/gui/vpProjectionDisplay.h>
+#include <visp3/robot/vpSimulatorCamera.h>
 #include <visp3/visual_features/vpFeatureBuilder.h>
-#include <visp3/visual_features/vpFeatureDepth.h>
-#include <visp3/visual_features/vpFeaturePoint.h>
 #include <visp3/vs/vpServo.h>
+#include <visp3/vs/vpServoDisplay.h>
+#include <visp3/gui/vpPlot.h>
 
-
-using namespace std;
-
-
-
-int main(int argc, char **argv)
+int main()
 {
-    vpHomogeneousMatrix cdMo;
-    cdMo[0][3] = 0.0;  // x
-    cdMo[1][3] = 0.0;  // y
-    cdMo[2][3] = 1.;  // z
-
-    vpHomogeneousMatrix cMo;
-    cMo[0][3] = 0.3;         // x
-    cMo[1][3] = cdMo[1][3];  // y
-    cMo[2][3] = 10.;         // z
-    vpRotationMatrix cRo(0, atan2(cMo[0][3], cMo[1][3]), 0);   // rot_y
-    cMo.insert(cRo);
-
-    vpFeaturePoint3D feature_x_z_;
-    vpFeaturePoint3D feature_x_z_d_;
-    feature_x_z_.buildFrom(cMo[0][3], cMo[1][3], cMo[2][3]);
-    feature_x_z_d_.buildFrom(cdMo[0][3], cdMo[1][3], cdMo[2][3]);
-
-    vpSimulatorPioneer robot;
-    robot.setSamplingTime(0.04);
-    vpHomogeneousMatrix wMc, wMo;
-    robot.getPosition(wMc);
-    wMo = wMc * cMo;
-
-    vpServo task;
-    task.setServo(vpServo::EYEINHAND_L_cVe_eJe);            // 应为用的是cMo 特征，发的速度确是相对于End-effector的，所以需要cam到end-effector的变换（速度变换和雅克比变换）
-    task.setInteractionMatrixType(vpServo::MEAN, vpServo::PSEUDO_INVERSE);
-    task.setLambda(0.2);
-    task.addFeature(feature_x_z_, feature_x_z_d_);  //, vpFeaturePoint3D::selectX() | vpFeaturePoint3D::selectZ()
-
-    vpVelocityTwistMatrix cVe;
-    cVe = robot.get_cVe();
-    task.set_cVe(cVe);
-
-    vpMatrix eJe;
-    robot.get_eJe(eJe);
-    task.set_eJe(eJe);
-
-
-    vpPlot graph(2, 800, 500, 400, 10, "Curves...");
-    graph.initGraph(0, 2); // v. w
-    graph.initGraph(1, 3); // error
-    graph.setTitle(0, "Velocities");
-    graph.setTitle(1, "Error s-s*");
-
-    graph.setLegend(0, 0, "vx");
-    graph.setLegend(0, 1, "wz");
-    graph.setLegend(1, 0, "x");
-    graph.setLegend(1, 1, "y");
-    graph.setLegend(1, 2, "z");
-
-    vpColVector v;
-    int n = 0;
-    // cMo
-    while(true)
-    {
-        robot.getPosition(wMc);
-        cMo = wMc.inverse() * wMo;
-
-        feature_x_z_.buildFrom(cMo[0][3], cMo[1][3], cMo[2][3]);
-
-        v = task.computeControlLaw();
-        vpColVector v_full;
-        v_full.resize(6, 1);
-        v_full[0] = v[0];
-        v_full[1] = 0.;
-        v_full[2] = 0.;
-        v_full[3] = 0.;
-        v_full[4] = 0.;
-        v_full[5] = v[1];
-
-        vpColVector v_cam = cVe * v_full;
-
-        vpColVector::saveYAML("v.dat", v_full);
-        vpColVector::saveYAML("v_cam.dat", v_cam);
-
-        break;
-
-        robot.setVelocity(vpRobot::ARTICULAR_FRAME, v);
-        graph.plot(0, n, v);
-        graph.plot(1, n, task.getError()); // plot error vector
-
-
-        double error = task.getError().sumSquare();
-        std::cout << "n: " << n++ << " v: " << v[0] <<" " << v[1] << " error: " << error << std::endl;
-
-        if (error < 0.0001) {
-            std::cout << "Reached a small error. We stop the loop... " << std::endl;
-            break;
+        vpHomogeneousMatrix cdMo(0, 0, 0.75, 0, 0, 0);
+        vpHomogeneousMatrix cMo(0.15, -0.1, 1., vpMath::rad(10), vpMath::rad(-10), vpMath::rad(50));
+        vpPoint point[4];
+        point[0].setWorldCoordinates(-0.1, -0.1, 0);
+        point[1].setWorldCoordinates(0.1, -0.1, 0);
+        point[2].setWorldCoordinates(0.1, 0.1, 0);
+        point[3].setWorldCoordinates(-0.1, 0.1, 0);
+        vpServo task;
+        task.setServo(vpServo::EYEINHAND_CAMERA);
+        task.setInteractionMatrixType(vpServo::CURRENT);
+        task.setLambda(0.5);
+        vpFeaturePoint p[4], pd[4];
+        for (unsigned int i = 0; i < 4; i++) {
+            point[i].track(cdMo);
+            vpFeatureBuilder::create(pd[i], point[i]);
+            point[i].track(cMo);
+            vpFeatureBuilder::create(p[i], point[i]);
+            task.addFeature(p[i], pd[i]);
         }
-    }
+        vpHomogeneousMatrix wMc, wMo;
+        vpSimulatorCamera robot;
+        robot.setSamplingTime(0.040);
+        robot.getPosition(wMc);
+
+        wMo = wMc * cMo;
+
+        //return 0;
+
+
+        vpPlot graph(1, 800, 500, 400, 10, "Curves...");
+
+        graph.initGraph(0, 6); // v. w
+        //graph.initRange(0, 0, 15, 0, 1);
+        graph.setTitle(0, "traj");
+        graph.setLegend(0, 0, "vx");
+        graph.setLegend(0, 1, "vy");
+        graph.setLegend(0, 2, "vz");
+        graph.setLegend(0, 3, "wx");
+        graph.setLegend(0, 4, "wy");
+        graph.setLegend(0, 5, "wz");
+
+
+
+        for (unsigned int iter = 0; iter < 150; iter++) {
+            robot.getPosition(wMc);
+            cMo = wMc.inverse() * wMo;
+            for (unsigned int i = 0; i < 4; i++) {
+                point[i].track(cMo);
+                vpFeatureBuilder::create(p[i], point[i]);
+            }
+            vpColVector v = task.computeControlLaw();
+            robot.setVelocity(vpRobot::CAMERA_FRAME, v);
+
+            graph.plot(0, iter, v);
+            //std::cout << v.size();
+
+
+        }
+        task.kill();
     getchar();
-    return 0;
 }
